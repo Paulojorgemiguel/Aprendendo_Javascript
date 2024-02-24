6de44db68bb76ad00321aaf929e3a6e0052423d9
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-02-24 20:08:00 +0100

    Particularidade  no javascript
    Incluindo particularidade no javascript.

---
+++ 3_terceiro_programa/js/javascript.js
@@ -0,0 +1,18 @@
+console.log("1 - particularidade.")
+
+console.log("2 - Particularidade")
+
+
+//Tipagem fraca 
+var a = 12 // variavel 
+
+a = "Teste"
+
+//Tipagem fraca 
+console.log(a)
+
+var s = "Teste"
+
+s = 12
+
+console.log(s)
