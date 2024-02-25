549276320ace39af66d32bfdc84259542bce00e9
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-02-25 21:00:31 +0100

    variavel no javascript
    Declaração de variaveis.

---
+++ 4_variavel_js/js/javascript.js
@@ -0,0 +1,34 @@
+var a = 12;
+
+console.log(a)
+
+a = "teste";
+
+console.log(a)
+
+var Nome = "Paulo";
+
+console.log(Nome)
+
+var SobreNome = "Jorge";
+
+console.log(SobreNome)
+
+var NomeCompleto = Nome + " " + SobreNome;
+
+console.log(NomeCompleto)
+
+
+var _adc = "";
+
+let b = 1;
+const c = 2;
+
+console.log(b)
+console.log(c)
+
+var pasta = "1,2,3,4,5";
+
+console.log(pasta)
+
+var nota;
