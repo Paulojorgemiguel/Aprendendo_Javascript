5564f414408ab82694b5fea266e356ebe853574f
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-02-26 21:31:20 +0100

    tipos de dados.
    tipos de dados no javascript.

---
+++ 5_number/js/javascript.js
@@ -0,0 +1,19 @@
+var numero = 5;
+
+console.log(numero)
+console.log(typeof numero)
+
+var  float = 3.5
+
+console.log(float)
+console.log(typeof float)
+
+var esc = "1234";
+
+console.log(esc)
+console.log(typeof esc)
+
+console.log(NaN)
+console.log(+Infinity)
+console.log(-Infinity)
+
