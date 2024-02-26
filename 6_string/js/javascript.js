82ef0dea8066a0adca3303b519d0c403444caa56
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-02-26 22:04:53 +0100

    String no javascript
    Usando string no js.

---
+++ 6_string/js/javascript.js
@@ -0,0 +1,25 @@
+var nome = "Paulo";
+
+console.log(nome)
+console.log(typeof nome)
+
+var sobrenome = "Jorge"
+
+console.log(sobrenome)
+console.log(typeof sobrenome)
+
+
+var NomeCompleto = nome + " " + sobrenome;
+
+console.log(NomeCompleto)
+console.log(typeof NomeCompleto)
+
+var frase = "rápido"
+
+console.log("Como diria o pai de um amigo o computador é um burro muito " + frase)
+
+document.write(`Vejá o meu desenvolvimento em "javascript"`)
+
+console.log("Essa linguagem de programação é linda para o " + nome + " Programa com ela.")
+
+
