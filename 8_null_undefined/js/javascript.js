e2febfd98bca74fd5fefa527e68f503d246a7761
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-02-27 16:55:22 +0100

    null e undefined

---
+++ 8_null_undefined/js/javascript.js
@@ -0,0 +1,26 @@
+console.log(nome)
+console.log(float)
+
+var nome = "Paulo";
+console.log(typeof nome)
+
+var pasta = null;
+
+console.log(pasta)
+console.log(typeof pasta)
+
+
+var sobrenome;
+
+console.log(sobrenome)
+console.log(typeof sobrenome)
+
+
+var float = 3.5;
+
+var nota = null;
+
+console.log(nota)
+console.log(typeof nota)
+
+
