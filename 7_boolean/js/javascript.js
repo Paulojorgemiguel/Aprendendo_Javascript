44f81ce9cbf46c521039c33c84c6052b6b18bd6b
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-02-27 16:28:24 +0100

    Boolean em javascript.
    O exercicios de hoje foi de boolean no pratica.

---
+++ 7_boolean/js/javascript.js
@@ -0,0 +1,12 @@
+var verdadeiro = true;
+
+console.log(verdadeiro)
+console.log(typeof verdadeiro)
+
+console.log(false)
+console.log(typeof false)
+
+var t = false;
+
+console.log(t)
+console.log(typeof t)
