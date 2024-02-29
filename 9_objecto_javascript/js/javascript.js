ffb3da076c2d82b82b0fe6b2e724a29dd5e8cfcb
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-02-29 08:50:27 +0100

    Objecto no javascript.

---
+++ 9_objecto_javascript/js/javascript.js
@@ -0,0 +1,21 @@
+var conj = {
+    nome: "Paulo",
+    idade: "22",
+    profissão: "Programador",
+    linguagens_favoritas: "Javascript, php,"
+};
+
+console.log(conj)
+console.log(typeof conj)
+
+console.log(conj.nome)
+console.log(conj.idade)
+console.log(conj.profissão)
+
+console.log("O Meu nome é: " + conj.nome)
+
+conj.nota = "23";
+
+console.log(conj.nota)
+
+console.log(conj)
