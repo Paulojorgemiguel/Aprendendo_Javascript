b35ee4774ddead007fd6ff18ae0e92013eaa34ef
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-03-01 15:07:45 +0100

    Arrays na pratica.

---
+++ 10_arrays/js/javascript.js
@@ -0,0 +1,25 @@
+//arrays
+var arr = [1,2,3,4,5,{teste: 4}];
+
+console.log(arr)
+
+arr[6] = "Teste";
+
+console.log(arr)
+
+var arr2 = [1,2,3];
+
+arr2[3] = 4; 
+
+console.log(arr2)
+console.log(typeof arr2)
+
+
+//objectos
+var obj = {
+   name: "Paulo",
+   age: 21,
+   profession: "Programmer", 
+} //Programmer significa programador(a)
+
+console.log(obj)
