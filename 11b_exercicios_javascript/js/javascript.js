32cf52c056c22d5310a61133c4e85e9bd8b02b68
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-03-04 22:15:43 +0100

    exercicio javascript
    Esse é um exercicio.

---
+++ 11b_exercicios_javascript/js/javascript.js
@@ -0,0 +1,20 @@
+console.log("Testando um if")
+
+if(false){
+    console.log("Ele é menor de idade.")
+}
+
+let numero = 16;
+
+if(numero > 14){
+    console.log("Aprovado")
+}else{
+    console.log("Reprovado")
+}
+
+let n = 5;
+
+if(n >= 5){
+    console.log("A nota é: " + n)
+}
+
