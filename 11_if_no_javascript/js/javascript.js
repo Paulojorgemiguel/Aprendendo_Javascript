c0deb486d2bc0fe377be92cb0d27db716f610658
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-03-04 18:43:31 +0100

    if no javascript
    Usando if no javascript.

---
+++ 11_if_no_javascript/js/javascript.js
@@ -0,0 +1,18 @@
+console.log("Representado um if no javascript")
+
+if(true){
+    console.log("Esse é um if no javscript.")
+}
+
+console.log("Depois do if no javascript.")
+
+
+let nota = 10; //*let* e *const* substitui o *var* para de clara variaveis
+
+if(nota > 9){
+    console.log("Aprovado com o valor: " + nota)
+}//Aqui Concatenei...
+
+
+
+
