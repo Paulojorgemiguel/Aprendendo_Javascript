252b9296a509322357063e655ba6b4cc6dc0bd45
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-03-11 12:05:00 +0100

    exercicios javascript
    criei essa pasta para exercicios para Aperfeiçoar o meu javascript...

---
+++ Exercicios_javascript/002/js/javascript.js
@@ -0,0 +1,48 @@
+console.log("Teste")
+
+let valor = 14;
+
+if(valor > 12){
+    console.log("Escreve o meu " + valor + " é maior que " + 12)
+}
+
+console.log(valor)
+console.log(typeof valor)
+
+
+let jogo = [{playstation: 1},{playstation: 2},{playstation: 3},{playstation: 4},{playstation: 5},{playstation: 6},]
+
+console.log(jogo[0])
+console.log(jogo[1])
+console.log(jogo[2])
+console.log(jogo[3])
+console.log(jogo[4])
+console.log(jogo[5])
+console.log(jogo)
+console.log(typeof jogo)
+
+
+let Gamer = {
+    nome: "Pauloshisui",
+    profissão: "Stream",
+    idade: 22,
+    jogo: "Free Fire, Striker Zone."
+}
+
+console.log("Esse é o meu nome -> " + Gamer.nome)
+console.log("Esse é a minha -> " + Gamer.profissão)
+console.log("Esse é a minha idade -> " + Gamer.idade)
+console.log("Esses são os meus jogo favoritos -> " + Gamer.jogo)
+console.log(Gamer)
+console.log(typeof Gamer)
+
+let v = true;
+
+console.log(v)
+console.log(typeof v)
+
+let f = false;
+
+console.log(f)
+console.log(typeof f)
+
