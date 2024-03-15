3f073f2ccd391030962ed01c01788a4b02db6f4c
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-03-15 11:28:10 +0100

    Exercicio na prática com javascript
    Exercicio na prática com javascript...

---
+++ Exercicios_javascript/003/js/javascript.js
@@ -0,0 +1,60 @@
+console.log("Javascript no console do google...")
+
+let nota = true;
+
+console.log(nota)
+console.log(typeof nota)
+
+let Nota = 11;
+
+if(Nota > 15){
+    console.log("A nota é: " + nota)
+}else{
+    console.log("A nota é " + false)
+}
+
+console.log(Nota)
+
+var valor = {
+    nota1: 8,
+    nota2: 9,
+    nota3: 10,
+    nota4: 11,
+    nota5: 12,
+    nota6: 13,
+    nota7: 14,
+    nota8: 15,
+}
+
+console.log(valor)
+console.log(typeof valor)
+
+
+console.log(valor.nota1)
+console.log(valor.nota2)
+console.log(valor.nota3)
+console.log(valor.nota4)
+console.log(valor.nota5)
+console.log(valor.nota6)
+console.log(valor.nota7)
+console.log(valor.nota8)
+
+let artigos = ["1-coca-cola","2-Fanta","3-Monster","4-Café",]
+
+console.log(artigos)
+
+console.log(artigos[0])
+console.log(artigos[1])
+console.log(artigos[2])
+console.log(artigos[3])
+
+//adicionei direitamente do console.log e não foi necessário declarar var... 
+console.log(artigos[0] = 23)
+
+document.write(`"O javascrit é uma linguagem de programação."`)
+
+console.log("Fim do exercicio na prática...")
+
+
+
+
