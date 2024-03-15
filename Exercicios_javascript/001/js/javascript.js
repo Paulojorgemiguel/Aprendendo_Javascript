9e85701d0cd1d005e4e9aeb429169d258f5c938d
Paulo jorge <paulojorgemiguel2001@gmail.com>
2024-03-15 11:54:08 +0100

    Update javascript.js

--- Exercicios_javascript/001/js/javascript.js
+++ Exercicios_javascript/001/js/javascript.js
@@ -1,22 +1,21 @@
 console.log("Testando um if")
 
-if(false){
+if (false) {
     console.log("Ele é menor de idade.")
 }
 
 let numero = 16;
 
-if(numero > 14){
+if (numero > 14) {
     console.log("Aprovado")
-}else{
+} else {
     console.log("Reprovado")
 }
 
 let n = 5;
 
-if(n >= 5){
+if (n >= 5) {
     console.log("A nota é: " + n)
 }
 
 console.log("Fim do exercicio na prática...")
-
